1f290aa57dbb454d3d34b2a078ce65dc92ff6b2a
EC2 Default User <ec2-user@ip-172-31-30-165.us-east-2.compute.internal>
2021-03-14 23:46:17 +0000

    HW 4 Complete

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -3,14 +3,14 @@ require 'monkey_patch'
 
 describe MoviesController do 
     fixtures :movies
-    describe 'Looking for movies with same director' do
-        it 'should not find movies - sad path' do 
+    describe 'Checking for movies with same director' do
+        it 'should not find movies - my sad path' do 
             expect(Movie).to receive(:find).with("1").and_return(movies(:mymovie_4))
             get :same_director, id: 1
             response.response_code.should == 302
             expect(response).to redirect_to movies_path
         end
-        it 'should find movies - happy path' do 
+        it 'should find movies - expected - happy path' do 
             expect(Movie).to receive(:same_director_movies).with(movies(:mymovie_1).director).and_return([movies(:mymovie_1), movies(:mymovie_2)])
             expect(Movie).to receive(:find).with(movies(:mymovie_1).id.to_s).and_return(movies(:mymovie_1))
             get :same_director, id: movies(:mymovie_1).id
@@ -18,16 +18,16 @@ describe MoviesController do
         end
     end
     
-    describe 'Testing create method' do
-        it 'should create movie' do
+    describe 'Testing: Create Method' do
+        it 'It should create the movie' do
             expect(Movie).to receive(:create!).with({'title':'mymovie_2', 'director':'test'}).and_return(movies(:mymovie_2))
             post :create, {:movie => {'title':'mymovie_2', 'director':'test'}}
             expect(flash[:notice]).to match (/was successfully created./)
         end
     end
     
-    describe 'Testing update method' do
-        it 'should update movie' do
+    describe 'Testing: Update Method' do
+        it 'It should update movie' do
             expect(Movie).to receive(:find).with(movies(:mymovie_2).id.to_s).and_return(movies(:mymovie_2))
             expect(movies(:mymovie_2)).to receive(:update_attributes!).with({'title':'mymovie_2', 'director':'test'}).and_return(movies(:mymovie_2))
             put :update, {:id => movies(:mymovie_2).id, :movie => {'title':'mymovie_2', 'director':'test'}}
@@ -35,16 +35,16 @@ describe MoviesController do
         end
     end
     
-    describe 'Testing edit method' do
-        it 'should edit movie' do
+    describe 'Testing: Edit method' do
+        it 'It should edit movie' do
             expect(Movie).to receive(:find).with(movies(:mymovie_2).id.to_s).and_return(movies(:mymovie_2))
             get :edit, id: movies(:mymovie_2).id
             response.response_code.should == 200
         end
     end
     
-    describe 'Testing show method' do
-        it 'should show movie' do
+    describe 'Testing: Show method' do
+        it 'It should show movie' do
             expect(Movie).to receive(:find).with(movies(:mymovie_2).id.to_s).and_return(movies(:mymovie_2))
             get :show, id: movies(:mymovie_2).id
             response.response_code.should == 200
@@ -52,8 +52,8 @@ describe MoviesController do
         end
     end
     
-    describe 'Testing destroy method' do
-        it 'should destroy movie' do
+    describe 'Testing: Destroy method' do
+        it 'It hould destroy movie' do
             expect(Movie).to receive(:find).with(movies(:mymovie_2).id.to_s).and_return(movies(:mymovie_2))
             expect(movies(:mymovie_2)).to receive(:destroy)
             delete :destroy, id: movies(:mymovie_2).id
@@ -62,15 +62,15 @@ describe MoviesController do
         end
     end
     
-    describe 'Testing new method' do
-        it 'should open page for new movie' do
+    describe 'Testing: New method' do
+        it 'It should open page for new movie' do
             get :new
             response.response_code.should == 200
         end
     end
     
-    describe 'Testing index method' do
-        it 'should get movies' do
+    describe 'Testing: Index method' do
+        it 'It should get movies' do
             expect(Movie).to receive(:all).and_return([movies(:mymovie_1), movies(:mymovie_2)])
             get :index
             response.response_code.should == 200
